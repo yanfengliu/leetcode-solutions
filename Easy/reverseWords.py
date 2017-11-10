2e3040ddbd576b0383a69ae7c913c5ea507721f4
Yanfeng Liu <yanfengliux@gmail.com>
2017-11-10 14:45:03 -0600

    Update reverseWords.py

--- Easy/reverseWords.py
+++ Easy/reverseWords.py
@@ -1,3 +1,10 @@
+# Given a string, you need to reverse the order of characters in each word within a sentence while 
+# still preserving whitespace and initial word order.
+
+# Example 1:
+# Input: "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
+
 class Solution(object):
     def reverseWords(self, s):
         """
