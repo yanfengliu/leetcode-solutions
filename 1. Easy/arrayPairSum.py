6f118f00c42115abf8853ba6600f418d18c12ac9
Yanfeng Liu <yanfengliux@gmail.com>
2017-11-26 16:47:32 -0600

    Update arrayPairSum.py

--- 1. Easy/arrayPairSum.py
+++ 1. Easy/arrayPairSum.py
@@ -4,6 +4,8 @@
 # Input: [1,4,3,2]
 # Output: 4
 # Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+
+# https://leetcode.com/problems/array-partition-i/description/
     
 class Solution(object):
     def arrayPairSum(self, nums):
